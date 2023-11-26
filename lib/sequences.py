a2df88622fa722b46d90e6199f9df76a7a569e9c
Samson Githinji <githinjisamson148@gmail.com>
2023-11-26 08:35:12 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+
+    if length == 0:
+        fibonacciSeries = []
+
+    elif length == 1:
+        fibonacciSeries = [0]
+
+    elif length == 2:
+        fibonacciSeries = [0, 1]
+
+    else:
+        # initialize fibonacciSeries with [0, 1]
+        fibonacciSeries = [0, 1]
+
+        while len(fibonacciSeries) < length:
+            # nextNumber is as result of adding lastTwoNumbers
+            nextNumber = fibonacciSeries[-1] + fibonacciSeries[-2]
+
+            # add to list
+            fibonacciSeries.append(nextNumber)
+
+    print(fibonacciSeries)
+
+
+# print_fibonacci(9)
